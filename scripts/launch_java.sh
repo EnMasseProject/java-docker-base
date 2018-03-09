eca1be9d6caf7639f3797930ea1a081773df8115
Ulf Lilleengen <lulf@redhat.com>
2018-03-09 11:33:09 +0100

    Export log level environment variable

--- scripts/launch_java.sh
+++ scripts/launch_java.sh
@@ -6,6 +6,7 @@ shift
 . /opt/run-java/dynamic_resources.sh
 
 export MALLOC_ARENA_MAX=2
+export LOG_LEVEL=${LOG_LEVEL:-info}
 
 # Make sure that we use /dev/urandom
 JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
