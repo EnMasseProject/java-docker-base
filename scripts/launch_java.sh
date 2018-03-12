c007c1ad6e51ab5bcc8501a82da0ac881b63a85b
Ulf Lilleengen <lulf@redhat.com>
2018-03-12 12:38:03 +0100

    Set log level as properties

--- scripts/launch_java.sh
+++ scripts/launch_java.sh
@@ -6,10 +6,11 @@ shift
 . /opt/run-java/dynamic_resources.sh
 
 export MALLOC_ARENA_MAX=2
-export LOG_LEVEL=${LOG_LEVEL:-info}
+
+LOG_LEVEL=${LOG_LEVEL:-info}
 
 # Make sure that we use /dev/urandom
-JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
+JAVA_OPTS="${JAVA_OPTS} -DLOG_LEVEL=${LOG_LEVEL} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
 
 JAVA_OPTS="$(adjust_java_options ${JAVA_OPTS})"
 
