2da4f9c7a110966b4a358eae45b467c2451e7e82
Dmitry Volodin <dmvolod@gmail.com>
2018-06-11 22:53:39 +0300

    Add JAVA_DEBUG and JAVA_DEBUG_PORt options to enable image remote debug

--- scripts/launch_java.sh
+++ scripts/launch_java.sh
@@ -9,9 +9,13 @@ export MALLOC_ARENA_MAX=2
 
 LOG_LEVEL=${LOG_LEVEL:-info}
 
+if [ "${JAVA_DEBUG}" == "true" ]; then
+  java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-8787}"
+fi
+
 # Make sure that we use /dev/urandom
 JAVA_OPTS="${JAVA_OPTS} -DLOG_LEVEL=${LOG_LEVEL} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
 
-JAVA_OPTS="$(adjust_java_options ${JAVA_OPTS})"
+JAVA_OPTS="$(adjust_java_options ${JAVA_OPTS}) ${java_debug_args}"
 
 exec java $JAVA_OPTS -jar $JAR $@
